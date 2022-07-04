ea3931830e949329432e4bca53803f88c0e82a83
Hitwich <104765135+Hitwich@users.noreply.github.com>
2022-07-04 10:54:19 -0300

    Update timer.js

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useEffect } from 'react';
 import ConvertTImer from '../../functions/convertTime';
 import * as styled from './styles'
 export default function Timer({seconds,setSeconds}){
